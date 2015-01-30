607769c98dd47c3b684d1b4c685d12df88ed13e2
Rafael Santos <rafa.santos.24@unitec.edu>
2015-01-29 21:22:08 -0600

    Tarea 1 - Programacion 3

---
+++ Desktop/RASR-24/Tareas U/Programacion III/Libreria/SDL2TestSimple-master/main.cpp
@@ -0,0 +1,85 @@
+
+#include<SDL.h>
+#include<SDL_image.h>
+#include<iostream>
+
+SDL_Window* window;
+SDL_Renderer* renderer;
+SDL_Event Event;
+SDL_Texture *background,*character;
+SDL_Rect rect_background,rect_character;
+
+int main( int argc, char* args[] )
+{
+    //Init SDL
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        return 10;
+    }
+    //Creates a SDL Window
+    if((window = SDL_CreateWindow("Image Loading", 100, 100, 500/*WIDTH*/, 250/*HEIGHT*/, SDL_WINDOW_RESIZABLE | SDL_RENDERER_PRESENTVSYNC)) == NULL)
+    {
+        return 20;
+    }
+    //SDL Renderer
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED );
+    if (renderer == NULL)
+    {
+        std::cout << SDL_GetError() << std::endl;
+        return 30;
+    }
+
+    //Init textures
+    int w=0,h=0;
+    background = IMG_LoadTexture(renderer,"fondo.png");
+    SDL_QueryTexture(background, NULL, NULL, &w, &h);
+    rect_background.x = 0;
+    rect_background.y = 0;
+    rect_background.w = w;
+    rect_background.h = h;
+
+    character = IMG_LoadTexture(renderer, "personaje.png");
+    SDL_QueryTexture(character, NULL, NULL, &w, &h);
+    rect_character.x = 50;
+    rect_character.y = 100;
+    rect_character.w = w;
+    rect_character.h = h;
+
+    //Main Loop
+    while(true)
+    {
+        while(SDL_PollEvent(&Event))
+        {
+            if(Event.type == SDL_QUIT)
+            {
+                return 0;
+            }
+            if(Event.type == SDL_KEYDOWN)
+            {
+                if(Event.key.keysym.sym == SDLK_d && rect_character.x <= 451)
+                    rect_character.x++;
+                if(Event.key.keysym.sym == SDLK_a && rect_character.x >= 1)
+                    rect_character.x--;
+                if(Event.key.keysym.sym == SDLK_w && rect_character.y >= 1)
+                    rect_character.y--;
+                if(Event.key.keysym.sym == SDLK_s && rect_character.y <= 201)
+                    rect_character.y++;
+            }
+
+            if(rect_character.x > rect_background.x)
+                std::cout << "Posicion X:" << rect_character.x << std::endl;
+            if(rect_character.y < rect_background.y)
+                std::cout << "Posicion Y:" << rect_character.y << std::endl;
+            if(rect_character.x > rect_background.h)
+                std::cout << "Posicion X:" << rect_character.x << std::endl;
+            if(rect_character.y < rect_background.w)
+                std::cout << "Posicion Y:" << rect_character.y << std::endl;
+        }
+
+        SDL_RenderCopy(renderer, background, NULL, &rect_background);
+        SDL_RenderCopy(renderer, character, NULL, &rect_character);
+        SDL_RenderPresent(renderer);
+    }
+
+	return 0;
+}
